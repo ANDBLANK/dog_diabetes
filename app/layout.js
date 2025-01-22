8435185b0613dc34b112ce4c377b8bbffac5e6c3
Leejoongjae123 <ljj3347@naver.com>
2025-01-22 21:35:45 +0900

    '123'

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <BasicNavbar />
+        {/* <BasicNavbar /> */}
 
         <NextUIProvider>{children}</NextUIProvider>
       </body>
