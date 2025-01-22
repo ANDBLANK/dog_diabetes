1380a38dbe72d5522af493c24f382fba14d4cac7
Leejoongjae123 <ljj3347@naver.com>
2025-01-22 21:10:45 +0900

    '123'

--- app/loading/page.js
+++ app/loading/page.js
@@ -1,6 +1,12 @@
-'use client'
-import {CircularProgress, Card, CardBody, CardFooter, Chip} from "@nextui-org/react";
-import {useState, useEffect} from "react";
+"use client";
+import {
+  CircularProgress,
+  Card,
+  CardBody,
+  CardFooter,
+  Chip,
+} from "@nextui-org/react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Loading() {
@@ -22,31 +28,19 @@ export default function Loading() {
   }, [setProgress]);
 
   return (
-    <Card className="" shadow="none">
-      <CardBody className="justify-center items-center pb-0">
-        <CircularProgress
-          classNames={{
-            svg: "w-36 h-36 drop-shadow-md",
-            indicator: "stroke-black",
-            track: "stroke-black/10",
-            value: "text-3xl font-semibold text-black",
-          }}
-          showValueLabel={true}
-          strokeWidth={4}
-          value={progress}
-        />
-      </CardBody>
-      <CardFooter className="justify-center items-center pt-0">
-        <Chip
-          classNames={{
-            base: "border-1 border-black/30",
-            content: "text-black/90 text-small font-semibold",
-          }}
-          variant="bordered"
-        >
-          분석중입니다
-        </Chip>
-      </CardFooter>
-    </Card>
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <CircularProgress
+        classNames={{
+          svg: "w-36 h-36 drop-shadow-md",
+          indicator: "stroke-black",
+          track: "stroke-black/10",
+          value: "text-3xl font-semibold text-black",
+        }}
+        showValueLabel={true}
+        strokeWidth={4}
+        value={progress}
+      />
+      <p className="text-2xl font-semibold text-black">분석중입니다</p>
+    </div>
   );
 }
